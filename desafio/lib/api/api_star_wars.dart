657bfde7f793a92ba9a385ab1e45c6f14f67651b
Pedro Augusto <pedroagvs@gmail.com>
2022-05-30 17:34:50 -0300

    salvando

--- desafio/lib/api/api_star_wars.dart
+++ desafio/lib/api/api_star_wars.dart
@@ -8,31 +8,23 @@ import '../pages/star_wars_page.dart';
 
 class StarWarApi {
   StarWarApi();
-  Future<UserStarWars> buscarPersonagens(int index) async {
-    final response = await http
-        .get(Uri.parse("https://swapi.dev/api/people/${index.toString()}/"));
+
+  Future<List<UserStarWars>> buscarPersonagens() async {
+    final response = await http.get(Uri.parse("https://swapi.dev/api/people/"));
+    List<UserStarWars> listaPersonagens = [];
     if (response.statusCode == 200) {
       final Map<String, dynamic> dados = json.decode(response.body);
-      final UserStarWars user = UserStarWars.fromJson(dados);
-      return user;
-    } else {
-      return UserStarWars(
-          name: "Erro", altura: "Erro", massa: "Erro", nascimento: "Erro");
+      final List<dynamic> dadosResults = dados['results'];
+      listaPersonagens =
+          dadosResults.map((dados) => UserStarWars.fromJson(dados)).toList();
     }
+
+    return listaPersonagens;
   }
 
-  List<UserStarWars> listUserStarWars = [];
   Future<Widget> loadUserStarWars() async {
-    int i = 1;
-    do {
-      UserStarWars user = await StarWarApi().buscarPersonagens(i);
-      user.id = i;
-      listUserStarWars.add(user);
-      i++;
-      print(user.name);
-    } while (i < 84);
     return Future.value(StarWarsPage(
-      listaPersonagens: listUserStarWars,
+      listaPersonagens: await StarWarApi().buscarPersonagens(),
     ));
   }
 }
